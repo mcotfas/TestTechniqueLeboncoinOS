e183853669723ec7402fb5560dbf9dae1c510293
Marius Cotfas <marius@cotfas.com>
2024-06-15 13:39:47 +0200

    Setup xcode project

---
+++ ListingLeboncoin/ListingLeboncoin/ViewController.swift
@@ -0,0 +1,23 @@
+//
+// Created by Marius Cotfas
+// 
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        view.backgroundColor = UIColor.blue
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    ViewController()
+}
+
+
---
+++ ListingLeboncoin/ListingLeboncoinTests/ListingLeboncoinTests.swift
@@ -0,0 +1,15 @@
+//
+// Created by Marius Cotfas
+// 
+//
+    
+
+import XCTest
+@testable import ListingLeboncoin
+
+final class ListingLeboncoinTests: XCTestCase {
+
+    func test_initialTest() {
+        XCTAssertTrue(true)
+    }
+}
